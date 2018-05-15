bc1abe9d750c4dd5e1bc9173c1040482c0d984b9
Adam <s165493@student.pg.edu.pl>
2018-05-15 17:32:21 +0200

    Basic map added

---
+++ Snowman/TileMap.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+#include "TileMap.h"
+
+TileMap::TileMap()
+{
+	tileset.loadFromFile("textures/tiles.png");
+	load_from_file("level.txt");
+	for(int i = 0; i < width; i++)
+		for (int j = 0; j < height; j++)
+		{
+			//get pointer to the current tile quad
+			sf::Vertex *quad = &vertices[(i + j * width) * 4];
+			quad[0].position = sf::Vector2f((float)i * TILE_WIDTH, (float)j * TILE_HEIGHT);
+			quad[1].position = sf::Vector2f((float)(i + 1) * TILE_WIDTH, (float)j * TILE_HEIGHT);
+			quad[2].position = sf::Vector2f((float)(i + 1) * TILE_WIDTH, (float)(j + 1) * TILE_HEIGHT);
+			quad[3].position = sf::Vector2f((float)i * TILE_WIDTH, (float)(j + 1) * TILE_HEIGHT);
+
+			select_texture(quad, tiles[i + j * width]);
+		}
+}
+
+TileMap::~TileMap()
+{
+
+}
+
+void TileMap::load_from_file(const std::string s)
+{
+	std::unique_ptr<std::fstream> fs{ new std::fstream{s, fs->in} };
+	if (!(*fs)) throw No_file;
+	*fs >> width >> height;
+	vertices.resize(width * height * 4);
+	vertices.setPrimitiveType(sf::Quads);
+
+	while (fs->good())
+	{
+		char tmp;
+		*fs >> tmp;
+		tiles.push_back(tmp);
+	}
+}
+
+void TileMap::select_texture(sf::Vertex* v, char tex)
+{
+	sf::Vector2f tmp;
+	switch (tex)
+	{
+	case '.':
+		tmp.x = 7, tmp.y = 4;
+		break;
+	case 'z':
+		tmp.x = 8, tmp.y = 12;
+		break;
+	default:
+		tmp.x = 0, tmp.y = 0;
+	}
+	v[0].texCoords = sf::Vector2f(tmp.x * TILE_WIDTH + 2 * tmp.x, tmp.y * TILE_HEIGHT + 2 * tmp.y);
+	v[1].texCoords = sf::Vector2f((tmp.x + 1) * TILE_WIDTH + 2 * tmp.x, tmp.y * TILE_HEIGHT + 2 * tmp.y);
+	v[2].texCoords = sf::Vector2f((tmp.x + 1) * TILE_WIDTH + 2 * tmp.x, (tmp.y + 1) * TILE_HEIGHT + 2 * tmp.y);
+	v[3].texCoords = sf::Vector2f(tmp.x * TILE_WIDTH + 2 * tmp.x, (tmp.y + 1) * TILE_HEIGHT + 2 * tmp.y);
+}
+
+void TileMap::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	states.transform *= getTransform();
+	states.texture = &tileset;
+	target.draw(vertices, states);
+}
+
+const int TileMap::TILE_HEIGHT = 70;
+
+const int TileMap::TILE_WIDTH = 70;
+
+const std::string TileMap::No_file = "No such file!";
---
+++ Snowman/TileMap.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "SFML\Graphics.hpp"
+#include <string>
+#include <vector>
+#include <memory>
+#include <fstream>
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+	TileMap();
+	~TileMap();
+private:
+	void load_from_file(const std::string s);
+	void select_texture(sf::Vertex* v, char tex);
+	std::vector<char> tiles;  // array of characters that representing tiles
+	sf::VertexArray vertices;
+	sf::Texture tileset;
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	static const int TILE_WIDTH;
+	static const int TILE_HEIGHT;
+	int width;
+	int height;
+	static const std::string No_file;
+protected:
+};
+
